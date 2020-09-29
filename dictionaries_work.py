40f6d510f923900c3271e3fc85e9274cfd3e8382
evachitul <eva.chitul@coleurope.eu>
2020-09-29 18:17:51 +0200

    '''Functions and dictionaries - homework'''

--- dictionaries_work.py
+++ dictionaries_work.py
@@ -11,7 +11,7 @@ raw_data = [
     ('BA', [': ', ': ', ': ', ': ', ': ', ': ', ': ', '69 ', '72 ']),
     ('BE', ['77 ', '78 ', '80 ', '83 ', '82 ', '85 ', '86 ', '87 ', '90 ']),
     ('BG', ['45 ', '51 ', '54 ', '57 ', '59 ', '64 ', '67 ', '72 ', '75 ']),
-    ('CH', [': ', ': ', ': ', '91 ', ': ', ': ', '93 b', ': ', '96 ']),
+    ('CH', [': ', ': ', ': ', '91 ', ': ', ': ', '93', ': ', '96 ']),
     ('CY', ['57 ', '62 ', '65 ', '69 ', '71 ', '74 ', '79 ', '86 ', '90 ']),
     ('CZ', ['67 ', '73 ', '73 ', '78 ', '79 ', '82 ', '83 ', '86 ', '87 ']),
     ('DE', ['83 ', '85 ', '88 ', '89 ', '90 ', '92 ', '93 ', '94 ', '95 ']),
@@ -28,8 +28,8 @@ raw_data = [
     ('IS', ['93 ', '95 ', '96 ', '96 ', ': ', ': ', '98 ', '99 ', '98 ']),
     ('IT', ['62 ', '63 ', '69 ', '73 ', '75 ', '79 ', '81 ', '84 ', '85 ']),
     ('LT', ['60 ', '60 ', '65 ', '66 ', '68 ', '72 ', '75 ', '78 ', '82 ']),
-    ('LU', ['91 ', '93 ', '94 ', '96 ', '97 ', '97 ', '97 ', '93 b', '95 ']),
-    ('LV', ['64 ', '69 ', '72 ', '73 ', '76 ', '77 b', '79 ', '82 ', '85 ']),
+    ('LU', ['91 ', '93 ', '94 ', '96 ', '97 ', '97 ', '97 ', '93', '95 ']),
+    ('LV', ['64 ', '69 ', '72 ', '73 ', '76 ', '77 ', '79 ', '82 ', '85 ']),
     ('ME', [': ', '55 ', ': ', ': ', ': ', ': ', '71 ', '72 ', '74 ']),
     ('MK', [': ', '58 ', '65 ', '68 ', '69 ', '75 ', '74 ', '79 ', '82 ']),
     ('MT', ['75 ', '77 ', '78 ', '80 ', '81 ', '81 ', '85 ', '84 ', '86 ']),
@@ -37,9 +37,9 @@ raw_data = [
     ('NO', ['92 ', '93 ', '94 ', '93 ', '97 ', '97 ', '97 ', '96 ', '98 ']),
     ('PL', ['67 ', '70 ', '72 ', '75 ', '76 ', '80 ', '82 ', '84 ', '87 ']),
     ('PT', ['58 ', '61 ', '62 ', '65 ', '70 ', '74 ', '77 ', '79 ', '81 ']),
-    ('RO', ['47 ', '54 ', '58 ', '61 b', '68 ', '72 ', '76 ', '81 ', '84 ']),
+    ('RO', ['47 ', '54 ', '58 ', '61', '68 ', '72 ', '76 ', '81 ', '84 ']),
     ('RS', [': ', ': ', ': ', ': ', '64 ', ': ', '68 ', '73 ', '80 ']),
-    ('SE', ['91 ', '92 ', '93 ', '90 ', '91 ', '94 b', '95 ', '93 ', '96 ']),
+    ('SE', ['91 ', '92 ', '93 ', '90 ', '91 ', '94 ', '95 ', '93 ', '96 ']),
     ('SI', ['73 ', '74 ', '76 ', '77 ', '78 ', '78 ', '82 ', '87 ', '89 ']),
     ('SK', ['71 ', '75 ', '78 ', '78 ', '79 ', '81 ', '81 ', '81 ', '82 ']),
     ('TR', [': ', '47 ', '49 ', '60 ', '70 ', '76 ', '81 ', '84 ', '88 ']),
@@ -47,17 +47,37 @@ raw_data = [
     ('XK', [': ', ': ', ': ', ': ', ': ', ': ', '89 ', '93 ', '93 ']),
 ]
 
+
 def prep_dataset(years, dataset):
-    country_dict = defaultdict(list)
     country_dict = {country: [] for country, *_ in dataset}
 
     length_years = len(years[1])
 
     for num in range(length_years):
         for country, elements in dataset:
-            country_dict[country].append({'year': years[1][num], 'coverage': elements[num]})
+            if elements[num] != ': ':
+                country_dict[country].append({'year': years[1][num], 'coverage': int(elements[num].strip())})
+            else:
+                country_dict[country].append({'year': years[1][num], 'coverage': 0})
 
     return country_dict
 
 
-print(prep_dataset(description, raw_data))
+full_dataset = prep_dataset(description, raw_data)
+print(full_dataset, '\n')
+
+year_dict = {year.strip(): [] for year in description[1]}
+print(year_dict, '\n')
+
+
+def year_data(dataset, year):
+    for country, element in dataset.items():
+        for entry in element:
+            if entry['year'].strip() == year:
+                year_dict[entry['year'].strip()].append((country, entry['coverage']))
+
+    return year_dict
+
+
+data_from_year = year_data(full_dataset, '2011')
+print(data_from_year)
