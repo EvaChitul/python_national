0e2f47bc74295afe6b8053377bf6a7727d373dd3
evachitul <eva.chitul@coleurope.eu>
2020-10-23 16:42:37 +0200

    Class inheritance

--- Own projects/practice_classes.py
+++ Own projects/practice_classes.py
@@ -26,3 +26,40 @@ for user in users:
 
 for user in users_objects:
     print(f' The user name is {user.first_name} {user.last_name}, he is {user.age} years old and works at {user.company}')
+
+
+class Student(object):
+
+    def __init__(self, first_name, last_name, age):
+        self._first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    @property
+    def first_name(self):
+        return self._first_name
+
+    @first_name.setter
+    def first_name(self, first_name):
+        if isinstance(first_name, int):
+            raise TypeError('Name must be string')
+        else:
+            self._first_name = first_name
+
+    def __repr__(self):
+        class_name = type(self).__name__
+        return '{} ({} {} - {}) [{}]'.format(
+             class_name, self.first_name,
+            self.last_name, self.age, id(self) )
+
+    def __str__(self):
+        return '{} {}, {}'.format(
+            self.first_name, self.last_name, self.age)
+
+
+eva = Student('Eva', 'Chitul', 31)
+print(eva.first_name)
+eva.first_name = 'Dana'
+print(eva.first_name)
+eva.first_name = 25
+print(eva.first_name)
